2aeda9928170ddefbb9358fc386ea00494676702
Nikita Ziganshin <nikitaziganshin@outlook.com>
2022-10-19 23:37:28 +0300

    Single layer init

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: false,
+  application: {
+    baseUrl,
+    name: 'Stargazer',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44376/',
+    redirectUri: baseUrl,
+    clientId: 'Stargazer_App',
+    responseType: 'code',
+    scope: 'offline_access Stargazer',
+    requireHttps: true,
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44376',
+      rootNamespace: 'Stargazer',
+    },
+  },
+} as Environment;
